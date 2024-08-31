4a03dc2515fc080244e6aa7a465206651a33f876
JonathanMiskinich <jonathanjosueprog@gmail.com>
2024-08-30 23:06:39 -0300

    commit

--- Interfaces/obj/Debug/net8.0/Interfaces.AssemblyInfo.cs
+++ Interfaces/obj/Debug/net8.0/Interfaces.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interfaces")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b18a9ed2dd92be94ed0b5fa42254a6fb428556")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b66533b04c22901bbd69e7a881c959e2caee4050")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interfaces")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interfaces")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
