7a6613f611deeecdb68b7122da0f11a33ac52117
JonathanMiskinich <jonathanjosueprog@gmail.com>
2024-10-21 06:14:29 -0300

    add LectorDeArchivos al patron chain

--- Patrones-de-Diseño/ObtencionDeDatos.cs
+++ Patrones-de-Diseño/ObtencionDeDatos.cs
@@ -18,7 +18,7 @@ using MetodologíasDeProgramaciónI;
 
 namespace ObtencionDeDatos
 {
-	public class LectorDeArchivos 
+	public class LectorDeArchivos : BaseManejadorDatos
 	{
 		
 		// El alumno deberá agregar la ruta correspondiente a su equipo donde haya guardado el archvo con los datos
@@ -27,21 +27,23 @@ namespace ObtencionDeDatos
 		
 		private StreamReader lector_de_archivos;
 		
-		public LectorDeArchivos():base(){
+		public LectorDeArchivos():base()
+		{
 			lector_de_archivos = new StreamReader(ruta_archivo);
 		}
 		
-		public double numeroDesdeArchivo(double max)
+		public override int ObtenerNumero(int max)
 		{
 			string linea = lector_de_archivos.ReadLine();
-			return Double.Parse(linea.Substring(0, linea.IndexOf('\t'))) * max;
+			return int.Parse(linea.Substring(0, linea.IndexOf('\t'))) * max;
 		}
 		
-		public string stringDesdeArchivo(int cant){
+		public override string ObtenerCadena(int cant)
+		{
 			string linea = lector_de_archivos.ReadLine();
 			linea = linea.Substring(linea.IndexOf('\t')+1);
 			cant = Math.Min(cant, linea.Length);
 			return linea.Substring(0, cant);
 		}
-	}
+    }
 }
